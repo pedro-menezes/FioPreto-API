d53538608a1c5d9e6a7375fc029a148dc6078dd6
pedro-menezes <menezes.pedro10@hotmail.com>
2021-05-31 13:07:57 -0300

    fix: 🐛 Verificacao se existe cnpj

--- src/services/salons/create.service.js
+++ src/services/salons/create.service.js
@@ -5,7 +5,8 @@ const { ApplicationError } = require('../../utils');
 const { messages } = require('../../helpers');
 
 module.exports.create = async (params) => {
-  const salonExists = await salonsRepository.get({ cnpj: params.cnpj });
+  const salonExists = await salonsRepository.get({ where: { cnpj: params.cnpj } });
+  console.log(salonExists);
   if (salonExists) {
     throw new ApplicationError(messages.alreadyExists('cnpj'), StatusCodes.CONFLICT);
   }
